87bb8e26eed4edbd4527601f16e3e75fc6215327
Sergio Serrano <serranohsa@hotmail.com>
2021-06-09 23:11:37 -0600

    add search function

--- public/js/search.js
+++ public/js/search.js
@@ -3,24 +3,30 @@ function search(productSearch) {
   console.log(productSearch);
 
   (async () => {
-    const rawResponse = await fetch("https://fakestoreapi.com/products");
+    const rawResponse = await fetch("https://fakestoreapi.com/products/category/" + productSearch);
     const content = await rawResponse.json();
     console.log(content);
-    // for (let i = 0; i < content.length; i++) {
-    //     content[i].id,
-    //     content[i].title,
-    //     content[i].price,
-    //     content[i].category,
-    //     content[i].description,
-    //     content[i].image
-    // }
-    let i = 0;
-    let id = null;
-    while (productSearch === content[i].title);
-    while (productSearch === content[i].title) {
-      id = content[i].id;
-      i++;
-    }
-    console.log(id);
+
   })();
+  setTimeout(() => { window.location.replace("/products.html?category=" + productSearch); }, 1000);
+  ;
+}
+
+function productsBySearch(id, title, price, category, description, image){
+    var product = document.createElement("div");
+    product.setAttribute('class', 'col');
+    product.innerHTML =
+    `
+    <div class="card h-100">
+    <img src="${image}" class="card-img-top" alt="..." />
+    <div class="card-body">
+        <h5 class="card-title">${title}</h5>
+        <p class="card-text">
+        ${description}
+        </p>
+    </div>
+    </div>
+
+`;
+  return product;
 }
